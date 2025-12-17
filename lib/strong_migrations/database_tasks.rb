a8a3b82a4aaa66815f297e249eef85d72bbeae8a
Jana Dvořáková <jana@webinargeek.com>
2025-12-17 09:57:29 +0000

    Backporting Rails 8 support

--- lib/strong_migrations/database_tasks.rb
+++ lib/strong_migrations/database_tasks.rb
@@ -1,7 +1,7 @@
 module StrongMigrations
   module DatabaseTasks
     # Active Record 7 adds version argument
-    def migrate(*args)
+    def migrate(...)
       super
     rescue => e
       if e.cause.is_a?(StrongMigrations::Error)
